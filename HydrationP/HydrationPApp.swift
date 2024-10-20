c26e57179ca1ffd0966aac47396ee7bda482a2e5
Razzan1al <raseeri24@twq.idserve.net>
2024-10-20 09:26:21 +0300

    Initial Commit

---
+++ HydrationP/HydrationPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationPApp.swift
+//  HydrationP
+//
+//  Created by Razzan on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydrationPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HydrationPTests/HydrationPTests.swift
@@ -0,0 +1,17 @@
+//
+//  HydrationPTests.swift
+//  HydrationPTests
+//
+//  Created by Razzan on 17/04/1446 AH.
+//
+
+import Testing
+@testable import HydrationP
+
+struct HydrationPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
